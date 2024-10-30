8295dc9186672fa9b9167604a59d69a1739d1e41
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-10-30 13:58:54 -0400

    fdsafd

--- viteProject/JS/main.js
+++ viteProject/JS/main.js
@@ -1,2 +1,11 @@
 //USE NODE VERSION 18!!!
 import "../CSS/style.css";
+document.querySelector(".color-swa[").addEventListener("click", function () {
+  if (document.body.classList.contains("day")) {
+    document.body.classList.add("night");
+    document.body.classList.remove("day");
+  } else {
+    document.body.classList.add("day");
+    document.body.classList.remove("night");
+  }
+});
