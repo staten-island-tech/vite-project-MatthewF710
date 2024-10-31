bddd41719c8896115d3c753df75db421bcfc7762
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-10-31 13:59:28 -0400

    sigmastench

--- viteProject/JS/main.js
+++ viteProject/JS/main.js
@@ -1,6 +1,11 @@
 //USE NODE VERSION 18!!!
 import "../CSS/style.css";
-document.querySelector(".color-swa[").addEventListener("click", function () {
+const DOMSelectors = {
+  button: document.querySelector(".btn"),
+  form: document.querySelector(".form"),
+  container: document.querySelector(".container"),
+};
+document.querySelector(".color-swap").addEventListener("click", function () {
   if (document.body.classList.contains("day")) {
     document.body.classList.add("night");
     document.body.classList.remove("day");
