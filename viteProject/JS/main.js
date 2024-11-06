80964f25389f08a84aa75b71b2cd37e31a6d0878
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-11-05 19:50:50 -0500

    card

--- viteProject/JS/main.js
+++ viteProject/JS/main.js
@@ -1,21 +1,20 @@
 //USE NODE VERSION 18!!!
 import "../CSS/style.css";
-console.log("e");
 import { cardarray } from "./arrays.js";
-function createCards(x) {
-  for (i = 0; i < x.length; i++) {
-    DOMSelectors.container.insertAdjacentHTML(
-      "beforeEnd",
-      `<div class="card"><h1 class="card-header">${x.title}}</h1><img src="${x.imageLink}" alt="${x.altText}" class="card-img"><p class="card-rating">Rating: ${x.rating}</p><p class="card-desc">${x.description}</p></div>`
-    );
-  }
-}
-createCards(cardarray);
 const DOMSelectors = {
   colorswapper: document.querySelector(".color-swap"),
   form: document.querySelector(".form"),
   container: document.querySelector(".container"),
 };
+function createCards(x) {
+  x.forEach((item) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeEnd",
+      `<div class="card"><h1 class="card-header">${item.title}}</h1><img src="${item.imageLink}" alt="${item.altText}" class="card-img"><p class="card-rating">Rating: ${item.rating}</p><p class="card-desc">${item.description}</p></div>`
+    )
+  );
+}
+createCards(cardarray);
 DOMSelectors.colorswapper.addEventListener("click", function () {
   if (document.body.classList.contains("day")) {
     document.body.classList.add("night");
